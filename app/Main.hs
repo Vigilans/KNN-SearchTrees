e088bd392981052f51bc083e0dfc0206196efa13
Vigilans <vigilans@foxmail.com>
2019-01-15 00:09:29 +0800

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,29 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import Data.NNS
+import Data.Metric
 import Data.Point
 import qualified Data.KdTree as KdTree
 import qualified Data.CoverTree as CoverTree
 
-points = Vector3d <$> [(1, 2, 3), (2, 0, -1), (-3, 5, 0)]
+knn :: (Searcher s, Metric (Pt s)) => Pt s -> s -> [(Pt s, Val s)]
+knn = kNearestNeighbors 2
+
+points = map Vector [[1, 2, 3], [2, 0, -1], [-3, 5, 0]] :: [Vector3d]
 labels = [0, 1, 0]
 entries = zip points labels
-query = Vector3d (1, 0, 1)
+query = Vector [1, 0, 1] :: Vector3d
 
 bruteForce = BruteForce entries
-result1 = kNearestNeighbors bruteForce 2 query
+result1 = knn query bruteForce
 
 kdTree = KdTree.fromList entries
-result2 = KdTree.kNearestNeighbors kdTree 2 query 
+result2 = knn query kdTree
 
-coverTree = CoverTree.fromList 10 entries
-result3 = kNearestNeighbors coverTree 2 query 
-
+coverTree = CoverTree.fromList 3 entries
+isValid = CoverTree.checkInvariant coverTree
+result3 = knn query coverTree
 
 main :: IO ()
 main = do
