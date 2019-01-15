754d29ace1ea1cbd291d5cc39eaa1a63ee5eba47
Vigilans <vigilans@foxmail.com>
2019-01-15 13:01:22 +0800

    Benchmark update

--- app/Main.hs
+++ app/Main.hs
@@ -4,31 +4,54 @@ module Main where
 import Data.NNS
 import Data.Metric
 import Data.Point
+import Data.Time
+import Data.List
+import Data.List.Split
 import qualified Data.KdTree as KdTree
 import qualified Data.CoverTree as CoverTree
+import System.Environment
 
-knn :: (Searcher s, Metric (Pt s)) => Pt s -> s -> [(Pt s, Val s)]
-knn = kNearestNeighbors 2
+-- points = map Vector [[1, 2, 3], [2, 0, -1], [-3, 5, 0]] :: [Vector3d]
+-- labels = [0, 1, 0]
+-- entries = zip points labels
+-- query = Vector [1, 0, 1] :: Vector3d
 
-points = map Vector [[1, 2, 3], [2, 0, -1], [-3, 5, 0]] :: [Vector3d]
-labels = [0, 1, 0]
-entries = zip points labels
-query = Vector [1, 0, 1] :: Vector3d
+testBruteForce :: IO Double
+testBruteForce = do 
+    content <- readFile $ "test/test_set.txt"
+    let rawData = reverse . splitOn "\t" <$> lines content 
+    let dataset = map (\(tag:point) -> (Vector $ map read point :: Vector3d, read tag :: Int)) rawData
+    let (testSet, trainSet) = splitAt (length dataset `div` 3) dataset
 
-bruteForce = BruteForce entries
-result1 = knn query bruteForce
+    let bruteForce = BruteForce trainSet
+    testOnDataset trainSet testSet bruteForce
 
-kdTree = KdTree.fromList entries
-result2 = knn query kdTree
+testKdTree :: IO Double
+testKdTree = do 
+    content <- readFile $ "test/test_set.txt"
+    let rawData = reverse . splitOn "\t" <$> lines content 
+    let dataset = map (\(tag:point) -> (Vector $ map read point :: Vector3d, read tag :: Int)) rawData
+    let (testSet, trainSet) = splitAt (length dataset `div` 3) dataset
+    
+    let kdTree = KdTree.fromList trainSet
+    testOnDataset trainSet testSet kdTree
 
-coverTree = CoverTree.fromList 3 entries
-isValid = CoverTree.checkInvariant coverTree
-result3 = knn query coverTree
+testCoverTree :: IO Double
+testCoverTree = do 
+    content <- readFile $ "test/test_set.txt"
+    let rawData = reverse . splitOn "\t" <$> lines content 
+    let dataset = map (\(tag:point) -> (Vector $ map read point :: Vector3d, read tag :: Int)) rawData
+    let (testSet, trainSet) = splitAt (length dataset `div` 3) dataset
+
+    let coverTree = CoverTree.fromList 3 trainSet
+    print $ "isTreeValid: " ++ (show $ CoverTree.checkInvariant coverTree)
+    testOnDataset trainSet testSet coverTree
 
 main :: IO ()
-main = do
-    let [a, b, c] = entries
-    let t = CoverTree.singleton 7 a
-    return ()
+main = return ()
+
+    
+
+
     
     
--- src/Data/NNS.hs
+++ src/Data/NNS.hs
@@ -7,15 +7,33 @@ module Data.NNS where
 
 import Data.Metric
 import Data.Maybe
+import Data.Time
+import Data.List
+import Data.Ord
+import qualified Data.Map as M
 import qualified Data.Heap as Q
+import qualified Data.MultiSet as MS
 
 class Searcher s where
     type Pt s :: *
     type Val s :: *
+   
+    -- nearNeighbors ::  (Metric p) => s p v -> Double -> p -> [(p, v)]
     kNearestNeighbors :: (Metric (Pt s)) => Int -> Pt s -> s -> [(Pt s, Val s)]
+    
     nearestNeighbor   :: (Metric (Pt s)) => Pt s -> s -> (Pt s, Val s)
     nearestNeighbor = (head .) . kNearestNeighbors 1
-    -- nearNeighbors ::  (Metric p) => s p v -> Double -> p -> [(p, v)]
+    
+    testOnDataset :: (Metric (Pt s), Eq (Val s), Ord (Val s)) => [(Pt s, Val s)] -> [(Pt s, Val s)] -> s -> IO Double
+    testOnDataset trainSet testSet searcher = do
+        let k = round $ sqrt $ genericLength trainSet -- theory value
+        acc <- return $ foldl (\acc (p, v) ->
+                    let knn = kNearestNeighbors k p searcher
+                        counter = MS.fromList $ map snd knn
+                        (pred, _) = maximumBy (comparing snd) $ MS.toOccurList counter
+                    in if pred == v then acc + 1 else acc
+                ) 0 testSet
+        return (acc / genericLength testSet)
 
 -- Brute force method with max k heap
 newtype BruteForce p v = BruteForce [(p, v)]
